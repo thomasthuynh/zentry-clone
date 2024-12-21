af7f02c5e9749db10f5a01d32a1850608e6773a6
ThomasHuynh <Thomas_TTH@live.com>
2024-12-21 13:15:42 -0500

    First commit :rocket:

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold text-blue-300">Welcome to Zentry</h1>
+    </main>
+  );
+};
+
+export default App;
